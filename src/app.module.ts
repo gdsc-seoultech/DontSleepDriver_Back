4f7e6ecee3b95d6081a272aa8bf93aa9dd5f1cb4
inhyeok <ss112d86@gmail.com>
2022-02-07 21:05:53 +0900

    Feat:Add Prisma Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
+import { AppController } from './app.controller';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
+import { PrismaClientExceptionFilter } from './common/exception/prisma-client-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
-  
 }
 bootstrap();
